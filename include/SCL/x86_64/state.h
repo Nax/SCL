9c6c7114b977d8d1f1ebf8930b7d50a183a22868
Nax <max.bacoux@gmail.com>
2016-04-23 19:57:51 +0200

    added r[8-15] register saving for x86_64

--- include/SCL/x86_64/state.h
+++ include/SCL/x86_64/state.h
@@ -5,7 +5,6 @@
 
 struct SCL_State_
 {
-    uint64_t    rip;
     uint64_t    rax;
     uint64_t    rbx;
     uint64_t    rcx;
@@ -14,6 +13,14 @@ struct SCL_State_
     uint64_t    rdi;
     uint64_t    rbp;
     uint64_t    rsp;
+    uint64_t    r8;
+    uint64_t    r9;
+    uint64_t    r10;
+    uint64_t    r11;
+    uint64_t    r12;
+    uint64_t    r13;
+    uint64_t    r14;
+    uint64_t    r15;
 };
 
 #endif
