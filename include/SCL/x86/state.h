9d9b04679650d7e527294c8b4b7ba3ae99b2b283
Nax <max.bacoux@gmail.com>
2016-04-24 18:05:21 +0200

    Huge refactor of x64 code, should be much faster and safe

--- include/SCL/x86/state.h
+++ include/SCL/x86/state.h
@@ -5,17 +5,7 @@
 
 struct SCL_State_
 {
-    uint32_t    eax;
-    uint32_t    ebx;
-    uint32_t    ecx;
-    uint32_t    edx;
-    uint32_t    esi;
-    uint32_t    edi;
-    uint32_t    ebp;
     uint32_t    esp;
-    uint32_t    dflags;
-    char        padding[8];
-    char        fxsave[512];
 };
 
 #endif
